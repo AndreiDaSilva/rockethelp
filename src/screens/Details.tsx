06165935a22c52c886ab878f54627a47164d8391
Andrei Robson da Silva <andreirobson@furb.br>
2022-07-21 02:31:50 -0300

    Create order type

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,20 +1,85 @@
-import { VStack } from 'native-base';
+import { HStack, VStack, useTheme, Text, ScrollView } from 'native-base';
 import React from 'react';
 import { Header } from '../components/Header';
 import { useRoute } from '@react-navigation/native';
+import { useEffect, useState } from 'react'
+import { OrderProps } from '../components/Order';
+import firestore from '@react-native-firebase/firestore'
+import { OrderFirestoreDTO } from '../DTOs/OrderFirestoreDTO';
+import { dateFormat } from '../utils/firestoreDateFormat';
+import { Loading } from '../components/Loading';
+import { CircleWavyCheck, Hourglass } from 'phosphor-react-native'
+
 
 
 type RouteParams = {
   orderId: string;
 }
 
+type OrderDetails = OrderProps & {
+  description: string;
+  solution: string;
+  closed: string;
+}
+
 export function Details() {
+  const [loading, setLoding] = useState(true);
+  const [order, setOrder] = useState<OrderDetails>({} as OrderDetails);
+  const [solution, setSolution] = useState('');
+
+  const { colors } = useTheme();
   const route = useRoute();
   const { orderId } = route.params as RouteParams;
 
+  useEffect(() => {
+    firestore()
+      .collection<OrderFirestoreDTO>('orders')
+      .doc(orderId)
+      .get()
+      .then((doc) => {
+        const { patrimony, description, status, created_at, closed_at, solution } = doc.data();
+        const closed = closed_at ? dateFormat(closed_at) : null;
+
+        setOrder({
+          id: doc.id,
+          patrimony,
+          description,
+          status,
+          solution,
+          when: dateFormat(created_at),
+          closed
+        });
+        setLoding(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <Loading />
+  }
+
   return (
+
     <VStack flex={1} bg="gray.700">
       <Header title="Solicitação" />
+      <HStack bg="gray.500" justifyContent="center" p={4}>
+        {
+          order.status === 'closed'
+            ? <CircleWavyCheck size={22} color={colors.green[300]} />
+            : <Hourglass size={22} color={colors.secondary[700]} />
+        }
+
+        <Text
+        fontSize="sm"
+        color={order.status === 'closed' ? colors.green[300] : colors.secondary[700]}
+        ml={2}
+        textTransform="uppercase"
+        >
+          {order.status === 'closed' ? 'finalizado' : 'em andamento'}
+        </Text>
+      </HStack>
+      <ScrollView mx={5} showsVerticalScrollIndicator={false}>
+        
+      </ScrollView>
     </VStack>
   );
 }
